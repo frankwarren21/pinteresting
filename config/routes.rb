5b1479261d8a0d634f0302753a0424bd82f784f2
frankwarren21 <frank.warren@gmail.com>
2017-02-24 16:11:05 -0500

    Created  home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Pinteresting::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
